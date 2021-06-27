4b7419f7ae47ead9b3360314d23cde7cf55f6cd6
Stephanie041996 <michellesakuhuni5@gmail.com>
2021-06-27 11:10:42 +0200

    Add create as root and add views

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   resources :words, only: [:index, :create, :new, :show]
-  root 'words#index'
+  root 'words#new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
