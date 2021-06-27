6ef05647554ae597a7f9c5c56b09f5641d05c87e
Stephanie041996 <michellesakuhuni5@gmail.com>
2021-06-27 11:19:12 +0200

    Add controller methods

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -1,2 +1,28 @@
 class WordsController < ApplicationController
+    def index
+        @words = Word.all
+    end
+    
+    def new
+        @word = Word.new
+    end
+
+    def show
+    end
+
+    def create
+        @word = Word.new(word_params)
+        if @word.save
+
+            redirect_to @word
+          else
+            render 'new'
+          end
+    end
+
+    private
+    def word_params
+      params.require(:word).permit(:word, :length, :sub_word)
+    end
+  
 end
