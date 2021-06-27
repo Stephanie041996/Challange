4cd9bc08735d3f3c07b178063652abcaa24746f0
Stephanie041996 <michellesakuhuni5@gmail.com>
2021-06-27 20:55:25 +0200

    Remove controller name in url

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :words
+  resources :words, only: %i[index new create]
+  resources :words, path: '', except: %i[index new create]
+  # get  '/:words' => 'words#show',
   root 'words#new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
